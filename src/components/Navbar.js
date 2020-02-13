fd8edcaeb3f6194de7146f8ae7eaa35f31ee3cd1
lluisguardiola <lluisguardiola.h@gmail.com>
2020-02-13 10:26:56 -0600

    added app title to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,30 @@
 import React from 'react'
-import {Box, Button, ButtonGroup, Grid} from "@chakra-ui/core";
+import {Box, Button, Icon} from "@chakra-ui/core"
+import {FaBook} from 'react-icons/fa'
 
 
 class Navbar extends React.Component {
 	render() {
-
-		// const styles = {
-		// 	positon: "absolute; right: 0"
-		// }
-
+		const h1styles = {
+			color: "white",
+			fontSize: "60px"
+		}
 		return (
 				<Box 
 					w="100%"
 					p={5}
 					bg="cyan.400"
 				>
+					<Box d="flex" alignItems="baseline">
+						<h1 style={h1styles}>FlatNote  </h1><Box color="#ffffff" as={FaBook} size="60px"/>
+					</Box>
+					<hr/>
+					<br/>
 					<Button 
 						variant="solid" 
 						backgroundColor="messenger.600" 
 						color="#ffffff"
 						size="md"
-						
 					>
 						Dashboard
 					</Button>
